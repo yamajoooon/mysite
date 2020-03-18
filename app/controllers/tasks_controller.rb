657de929f51a82293d27f9d5f461312d37d6c437
tennisfast <metarusword1998@gmail.com>
2020-03-19 01:37:09 +0900

    first-commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = current_user.tasks.order(created_at: :desc)
+  end
+
+  def show
+    #@task = current_user.tasks.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    #@task = current_user.tasks.find(params[:id])
+  end
+
+  def update
+    #task = current_user.tasks.find(params[:id])
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスクを「#{@task.name}」を更新しました。"
+    
+  end
+
+  def destroy
+    
+    @task.destroy
+    redirect_to tasks_url, notice: "タスクを「#{@task.name}」を削除しました。"
+    
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)   
+    if @task.save
+      redirect_to @task, notice:"タスク「#{@task.name}」を登録しました"
+    else
+      render :new
+    end
+    
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:description)
+    
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+    
+  end
+  
+end
---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ApplicationRecord
+    validates :name, presence: true
+
+    belongs_to :user
+
+    scope :recent, -> { order(created_at: :desc)}
+end
